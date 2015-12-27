6070dc0433dd717cba989da3610158bcc078a701
Parveen-Arora <parveen.arora@jabong.com>
2015-12-27 08:02:53 +0530

    Changing Session Expire time to 45 min

--- config/session.js
+++ config/session.js
@@ -29,11 +29,11 @@ module.exports.session = {
   *                                                                          *
   * Set the session cookie expire time The maxAge is set by milliseconds,    *
   * the example below is for 24 hours                                        *
-  *                                                                          *
+  * Changing Session Expire time to 45 min                                   *
   ***************************************************************************/
 
    cookie: {
-     maxAge: 24 * 60 * 60 * 1000
+     maxAge: 30 * 60 * 1000
    },
 
   /***************************************************************************
